91522b991ac86d50cae38c7130dfba0077c0bff1
r.kazakov <kazakov302215@gmail.com>
2021-11-29 23:50:46 +0300

    eslint config

--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,23 @@ console.log('prod', isProd)
 
 const fileName = extension => (isDev ? `bundle.${extension}` : `bundle.[hash].${extension}`)
 
+const jsLoaders = () => {
+  const loaders = [
+    {
+      loader: 'babel-loader',
+      options: {
+        presets: ['@babel/preset-env']
+      }
+    }
+  ]
+
+  if (isDev) {
+    loaders.push('eslint-loader')
+  }
+
+  return loaders
+}
+
 module.exports = {
   context: path.resolve(__dirname, 'src'),
   mode: 'development',
@@ -57,12 +74,7 @@ module.exports = {
       {
         test: /\.m?js$/,
         exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env']
-          }
-        }
+        use: jsLoaders()
       }
     ]
   }
