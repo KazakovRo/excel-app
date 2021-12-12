d238ceeb1a87486f8d609f1dd8e8b95b7b4845e6
r.kazakov <kazakov302215@gmail.com>
2021-12-13 00:06:38 +0300

    added table cells

--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -3,11 +3,11 @@ const CODES = {
   Z: 90
 }
 
-// function createCell() {
-//   return `
-//     <div class="cell" contenteditable>B2</div>
-//   `
-// }
+function createCell() {
+  return `
+    <div class="cell" contenteditable></div>
+  `
+}
 
 function createCol(col) {
   return `
@@ -24,7 +24,7 @@ function createRow(content) {
   `
 }
 
-function toChar(_, index) {
+function allLetters(_, index) {
   return String.fromCharCode(CODES.A + index)
 }
 
@@ -32,16 +32,48 @@ export function createTable(rowsCount = 15) {
   const colsCount = CODES.Z - CODES.A + 1
   const rows = []
 
-  const ArrayOfLetetrs = new Array(colsCount).fill('').map(toChar) // new Array(colsCount).fill('').map((elem, index) => String.fromCharCode(CODES.A + index))
+  const ArrayOfLetetrs = new Array(colsCount).fill('').map(allLetters) // new Array(colsCount).fill('').map((elem, index) => String.fromCharCode(CODES.A + index))
   const cols = ArrayOfLetetrs.map(createCol).join('') // ArrayOfLetetrs.map(elem => createCol(elem)).join('')
-
-  console.log(cols)
 
   rows.push(createRow(cols))
 
   for (let i = 0; i < rowsCount; i++) {
-    rows.push(createRow())
+    const cells = new Array(colsCount).fill('').map(createCell).join('')
+
+    rows.push(createRow(cells))
   }
 
   return rows.join('')
 }
+
+// {
+/* <div class="row">
+  <div class="row-info"></div>
+
+  <div class="row-data">
+    <div class="column">A</div>
+    <div class="column">B</div>
+    <div class="column">C</div>
+  </div>
+</div>
+
+<div class="row">
+  <div class="row-info">1</div>
+
+  <div class="row-data">
+    <div class="cell selected" contenteditable>123213</div>
+    <div class="cell" contenteditable>123132132123</div>
+    <div class="cell" contenteditable>222</div>
+  </div>
+</div>
+
+<div class="row">
+  <div class="row-info">2</div>
+
+  <div class="row-data">
+    <div class="cell">123213</div>
+    <div class="cell">123132132123</div>
+    <div class="cell">222</div>
+  </div>
+</div> */
+// }
