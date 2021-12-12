6a626aeb4dc3d1911298fab80dfc4c0cc49e3e7d
r.kazakov <kazakov302215@gmail.com>
2021-12-13 00:10:31 +0300

    added first column numbers

--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -15,10 +15,10 @@ function createCol(col) {
   `
 }
 
-function createRow(content) {
+function createRow(index, content) {
   return `
     <div class="row">
-      <div class="row-info"></div>
+      <div class="row-info">${index ? index : ''}</div>
       <div class="row-data">${content}</div>
     </div>
   `
@@ -35,45 +35,13 @@ export function createTable(rowsCount = 15) {
   const ArrayOfLetetrs = new Array(colsCount).fill('').map(allLetters) // new Array(colsCount).fill('').map((elem, index) => String.fromCharCode(CODES.A + index))
   const cols = ArrayOfLetetrs.map(createCol).join('') // ArrayOfLetetrs.map(elem => createCol(elem)).join('')
 
-  rows.push(createRow(cols))
+  rows.push(createRow(null, cols))
 
   for (let i = 0; i < rowsCount; i++) {
     const cells = new Array(colsCount).fill('').map(createCell).join('')
 
-    rows.push(createRow(cells))
+    rows.push(createRow(i + 1, cells))
   }
 
   return rows.join('')
 }
-
-// {
-/* <div class="row">
-  <div class="row-info"></div>
-
-  <div class="row-data">
-    <div class="column">A</div>
-    <div class="column">B</div>
-    <div class="column">C</div>
-  </div>
-</div>
-
-<div class="row">
-  <div class="row-info">1</div>
-
-  <div class="row-data">
-    <div class="cell selected" contenteditable>123213</div>
-    <div class="cell" contenteditable>123132132123</div>
-    <div class="cell" contenteditable>222</div>
-  </div>
-</div>
-
-<div class="row">
-  <div class="row-info">2</div>
-
-  <div class="row-data">
-    <div class="cell">123213</div>
-    <div class="cell">123132132123</div>
-    <div class="cell">222</div>
-  </div>
-</div> */
-// }
