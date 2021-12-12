1e68a3e1ce65607c2d34b33e8d8a1ed156b812cc
r.kazakov <kazakov302215@gmail.com>
2021-12-12 18:20:34 +0300

    toolbar click btns

--- src/components/toolbar/Toolbar.js
+++ src/components/toolbar/Toolbar.js
@@ -3,6 +3,17 @@ import { ExcelComponent } from '@core/ExcelComponent'
 export class Toolbar extends ExcelComponent {
   static className = 'excel__toolbar'
 
+  constructor($root) {
+    super($root, {
+      name: 'Toobar',
+      listeners: ['click']
+    })
+  }
+
+  onClick(e) {
+    console.log(e.target)
+  }
+
   toHTML() {
     return `
       <div class="button">
