56425a13f4b982e45dd9088e278601a17b1376d4
r.kazakov <kazakov302215@gmail.com>
2021-12-20 02:04:53 +0300

    encapsulated resize logic

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,6 +1,6 @@
 import { ExcelComponent } from '@core/ExcelComponent'
-import { library } from '@core/dom.js'
 import { createTable } from './table.template'
+import { resizeHandler } from './table.resize'
 
 export class Table extends ExcelComponent {
   static className = 'excel__table'
@@ -14,32 +14,9 @@ export class Table extends ExcelComponent {
   onMousedown(e) {
     const tableItem = e.target.dataset.resize
 
+    // shouldResize(e) --> equals -->  tableItem
     if (tableItem) {
-      // console.log('Start resizing', e.target.dataset.resize)
-      const $targetResize = library(e.target)
-      // add $el before closest and use native closest method
-      const $parentCell = $targetResize.closest('[data-type="resizable"]')
-      const coords = $parentCell.getCoords()
-      const dataCells = this.$root.findAll(`[data-col="${$parentCell.data.col}"]`)
-
-      // .data it's getter function
-      // console.log($parentCell.data.col)
-
-      document.onmousemove = e => {
-        const isColumn = tableItem === 'col'
-
-        const delta = isColumn ? e.pageX - coords.right : e.pageY - coords.bottom
-        const value = isColumn ? coords.width + delta : coords.height + delta
-        const styleSize = isColumn ? 'width' : 'height'
-
-        $parentCell.css({ [styleSize]: value + 'px' }) // $parentCell.$el.style[styleSize] = value + 'px'
-
-        dataCells.forEach(el => (el.style.width = value + 'px'))
-      }
-
-      document.onmouseup = () => {
-        document.onmousemove = null
-      }
+      resizeHandler(this.$root, e, tableItem)
     }
   }
 
